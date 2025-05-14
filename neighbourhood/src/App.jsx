e0f660e279eb0945a48c42e910ca2bcddf945dab
Julia Kleresjö <92084007+kleresjo@users.noreply.github.com>
2025-05-14 13:53:45 +0200

    Added productlist
    Added basic styling and component with static products from API

---
+++ neighbourhood/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PorudctsList from './components/ProductList';
+import ProductList from './components/ProductList';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Nya produkter</h1>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
---
+++ neighbourhood/src/components/ProductList.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { IoIosArrowRoundForward } from "react-icons/io";
+import { TiStar } from "react-icons/ti";
+
+function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+       fetch('/products.json')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error('Fel vid hämtning:', error));
+  }, []);
+
+  return (
+    <article className='wrapper'>
+      <div className='APItitle'>
+      <h2>Nya produkter</h2>
+      <p><IoIosArrowRoundForward className='rightArrow'/></p>
+      </div>
+      <ul className='APIList'>
+        {products.map(product => 
+          <li key={product.id} className='APIproduct'>
+                <img src={product.productImg} alt={product.productDes} />
+                <div className='APItitlePlacing'>
+                <div>
+            <h3 className='APIh3'>{product.productName}</h3>
+            <p className='APIp'>{product.productRenter}</p>
+            </div>
+            <div className='rating'>
+            <p className='ratingP'><TiStar className='star'/>{product.productRating} {product.productAmountRating}</p>
+            </div>
+            </div>
+          </li>
+        )}
+      </ul>
+    </article>
+  );
+}
+
+export default ProductList;
