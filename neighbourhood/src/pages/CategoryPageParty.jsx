c52eafa4f4d4fd0c4b3b0fb8ac1775df8016c8f2
Julia Kleresjö <92084007+kleresjo@users.noreply.github.com>
2025-05-17 15:59:34 +0200

    Added categories and links
    Added categories and links to different pages and the page for all product which "se alla kategorier" will be linked to for now, can be updated in future

---
+++ neighbourhood/src/pages/CategoryPageParty.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CategoryPageParty = () => {
+  return (
+    <div>
+      <div className='wrapper'>
+        <p>Party</p>
+    </div>
+    </div>
+  );
+};
+
+export default CategoryPageParty;
---
+++ neighbourhood/src/pages/ProductPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProductPage = () => {
+  return (
+    <div>
+      <div className='wrapper'>
+        <p>Alla produkter</p>
+    </div>
+    </div>
+  );
+};
+
+export default ProductPage;
