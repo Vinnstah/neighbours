73928e5a6c8fff3a64ddc30e00f1d31e55c3f7b4
Julia Kleresjö <92084007+kleresjo@users.noreply.github.com>
2025-05-26 20:05:35 +0200

    Created register account page

--- neighbourhood/src/pages/RegisterPage.jsx
+++ neighbourhood/src/pages/RegisterPage.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
     <div>
       <div className='loginWrapper'>
-        <p>reigsrer</p>
+         <img src={logo} alt="neighbours logga" />
+         <p>Förnamn</p>
+         <input type="text" name="" id="" placeholder='...' className='register-bar'/>
+         <p>Efternamn</p>
+         <input type="text" placeholder='...' className='register-bar'/>
+         <p>Mailadress</p>
+         <input type="text" placeholder='...' className='register-bar'/>
+         <p>Personnummer</p>
+         <input type="text" placeholder='...' className='register-bar'/>
+         <p>Gatuadress</p>
+         <input type="text" placeholder='...' className='register-bar'/>
+         <p>Postnummer</p>
+         <input type="text" placeholder='...' className='register-bar'/>
+         <p>Stad</p>
+         <input type="text" placeholder='...' className='register-bar'/>
+          <Link to="/" className="cat-link"><button  className='logInBtn'>Skapa konto</button></Link>
         </div>
     </div>
   );
